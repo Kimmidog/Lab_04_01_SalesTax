57d9320da095f6f6e2109e86cbbfbeeca9a0e213
Kimmidog <Kimmidog4@gmail.com>
2024-09-24 18:19:22 -0400

    Initial commit

---
+++ src/main.java
@@ -0,0 +1,11 @@
+
+public class main {
+    public static void main(String[] args) {
+        double price = 100.00; // random value i picked
+        double salesTax = price * 0.05;
+
+        // price and the computed sales tax
+        System.out.println("Price of the purchase: " + price);
+        System.out.println("Computed sales tax (5%): " + salesTax);
+    }
+}
